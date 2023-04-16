3a377049c18cfb494dc5fefbd57710c892f813dd
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-04-16 20:15:30 +0000

    YESS

--- program.py
+++ program.py
@@ -1,1 +1,8 @@
 #Create a program that finds the two-digit positive integer that becomes 7 times smaller when its left-most digit is removed.
+
+for x in range (10, 100):
+    z = int(x % 10)
+    if x / 7 == z:
+        print(x)
+
+
